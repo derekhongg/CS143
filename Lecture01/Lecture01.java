c6e3aaf5b50f2ca3f9663e0c638371398ee8ab96
Derek Hong <derek.hong@paccar.com>
2024-03-27 09:02:44 -0700

    file started

---
+++ Lecture01/Lecture01.java
@@ -0,0 +1,38 @@
+class Lecture01 {
+    public static void main(String[] args) {
+        System.out.println("Hello from lecture 01");
+
+        // SELF-CHECK PROBLEMS:
+        // Note: you may need to put some of your answers inside comments
+
+        // Put your answer for #1 here:
+        //
+        // Copy and paste the output from when you first cloned (downloaded) and ran
+        // this program
+        // (in other words, when you run this program, what does it print?
+        // Copy that output into this file, right here, in a comment
+
+        // Where in the textbook are the main topics for this question covered?
+
+        // Put your answer for #2 here:
+        // Where in the textbook are the main topics for this question covered?
+
+        // Put your answer for #3 here:
+        // Where in the textbook are the main topics for this question covered?
+
+        // Put your answer for #4 here:
+        // Where in the textbook are the main topics for this question covered?
+
+        // Put your answer for #5 here:
+        // Where in the textbook are the main topics for this question covered?
+
+        // Put your answer for #6 here:
+        // Where in the textbook are the main topics for this question covered?
+        // NOTE: The definition of the Food class should go OUTSIDE the Lecture01
+        // class. A good place to put this is at the end of the file.
+        // NOTE 2: Make sure that you create a couple of Food objects here in main()
+
+    }
+}
+
+// Placing the Food class below this line is a good place for it
