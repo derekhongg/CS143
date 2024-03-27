0200b7d292d6ffd8c9ac6d1272b8f968e8fabfba
Derek Hong <derek.hong@paccar.com>
2024-03-27 15:41:41 -0700

    new content added

--- Lecture01/Lecture01.java
+++ Lecture01/Lecture01.java
@@ -1,3 +1,8 @@
+//Derek Hong
+//CS 143
+
+import java.util.Scanner;
+
 class Lecture01 {
     public static void main(String[] args) {
         System.out.println("Hello from lecture 01");
@@ -6,7 +11,8 @@ class Lecture01 {
         // Note: you may need to put some of your answers inside comments
 
         // Put your answer for #1 here:
-        //
+        // Hello from lecture 01
+
         // Copy and paste the output from when you first cloned (downloaded) and ran
         // this program
         // (in other words, when you run this program, what does it print?
@@ -16,15 +22,41 @@ class Lecture01 {
 
         // Put your answer for #2 here:
         // Where in the textbook are the main topics for this question covered?
+        /*
+            for(int i = 1; i <= 100; i++) {
+                System.out.println(i);
+            }
 
+            I chose a for loop because it's fairly simple and paints everything out
+        */
         // Put your answer for #3 here:
         // Where in the textbook are the main topics for this question covered?
 
+        /*
+            for(int i = 1; i <= 5; i++){
+                for(int j = i; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        */
         // Put your answer for #4 here:
         // Where in the textbook are the main topics for this question covered?
-
+        /*
+            for(int i = 0; i <= n; i++) {
+                for(int j = i; j < n; j++) {
+                    System.out.print(n);
+                }
+                System.out.println();
+            }
+        */
         // Put your answer for #5 here:
         // Where in the textbook are the main topics for this question covered?
+
+        /*
+            See below
+        */
 
         // Put your answer for #6 here:
         // Where in the textbook are the main topics for this question covered?
@@ -32,6 +64,29 @@ class Lecture01 {
         // class. A good place to put this is at the end of the file.
         // NOTE 2: Make sure that you create a couple of Food objects here in main()
 
+    }
+    public class AverageGrades {
+        public static void main (String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("How many grades would you like to store? ");
+            int numGrades = input.nextInt();
+
+            double[] grades = new double[numGrades];
+            for(int i = 0; i < numGrades; i++) {
+                System.out.print("Next grade? ");
+                grades[i] = input.nextDouble();
+            }
+
+            double sum = 0;
+            for(int i = 0; i < grades.length; i++) {
+                sum += grades[i];
+            }
+
+            double avg = sum / numGrades;
+            System.out.println("The average of your grades is: " + avg);
+            input.close();
+        }
     }
 }
 
