a57e1c8f6c4c7997c7f090852edd921ef750f4b4
derekhongg <hongderek@gmail.com>
2024-03-28 20:03:46 -0700

    new stuff added

--- Lecture01/Lecture01.java
+++ Lecture01/Lecture01.java
@@ -23,40 +23,40 @@ class Lecture01 {
         // Put your answer for #2 here:
         // Where in the textbook are the main topics for this question covered?
         /*
-            for(int i = 1; i <= 100; i++) {
-                System.out.println(i);
-            }
-
-            I chose a for loop because it's fairly simple and paints everything out
-        */
+         * for(int i = 1; i <= 100; i++) {
+         * System.out.println(i);
+         * }
+         * 
+         * I chose a for loop because it's fairly simple and paints everything out
+         */
         // Put your answer for #3 here:
         // Where in the textbook are the main topics for this question covered?
 
         /*
-            for(int i = 1; i <= 5; i++){
-                for(int j = i; j <= i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-
-        */
+         * for(int i = 1; i <= 5; i++){
+         * for(int j = i; j <= i; j++) {
+         * System.out.print("*");
+         * }
+         * System.out.println();
+         * }
+         * 
+         */
         // Put your answer for #4 here:
         // Where in the textbook are the main topics for this question covered?
         /*
-            for(int i = 0; i <= n; i++) {
-                for(int j = i; j < n; j++) {
-                    System.out.print(n);
-                }
-                System.out.println();
-            }
-        */
+         * for(int i = 0; i <= n; i++) {
+         * for(int j = i; j < n; j++) {
+         * System.out.print(n);
+         * }
+         * System.out.println();
+         * }
+         */
         // Put your answer for #5 here:
         // Where in the textbook are the main topics for this question covered?
 
         /*
-            See below
-        */
+         * See below
+         */
 
         // Put your answer for #6 here:
         // Where in the textbook are the main topics for this question covered?
@@ -65,27 +65,83 @@ class Lecture01 {
         // NOTE 2: Make sure that you create a couple of Food objects here in main()
 
     }
+
     public class AverageGrades {
-        public static void main (String[] args) {
+        public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
 
             System.out.print("How many grades would you like to store? ");
             int numGrades = input.nextInt();
 
             double[] grades = new double[numGrades];
-            for(int i = 0; i < numGrades; i++) {
+            for (int i = 0; i < numGrades; i++) {
                 System.out.print("Next grade? ");
                 grades[i] = input.nextDouble();
             }
 
             double sum = 0;
-            for(int i = 0; i < grades.length; i++) {
+            for (int i = 0; i < grades.length; i++) {
                 sum += grades[i];
             }
 
             double avg = sum / numGrades;
             System.out.println("The average of your grades is: " + avg);
             input.close();
+        }
+    }
+
+    public class Food {
+        private String name;
+        private int calories;
+        private double rating;
+
+        public Food(String name, int calories, double rating) {
+            this.name = name;
+            this.calories = calories;
+            this.rating = rating;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getCalories() {
+            return calories;
+        }
+
+        public void setCalories(int calories) {
+            this.calories = calories;
+        }
+
+        public double getRating() {
+            return rating;
+        }
+
+        public void setRating(double rating) {
+            if (rating >= 0 && rating <= 5) {
+                this.rating = rating;
+            } else {
+                System.out.println("Rating must be between 0 and 5.");
+            }
+        }
+
+        public void print() {
+            System.out.println("Name: " + name);
+            System.out.println("Calories: " + calories);
+            System.out.println("Rating: " + rating);
+        }
+
+        public static void main(String[] args) {
+            Food food1 = new Food("Candy", 107, 4.5);
+
+            System.out.println("Attributes of food1:");
+            food1.print();
+            System.out.println();
+
         }
     }
 }
