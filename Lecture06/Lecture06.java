4ef214e73da2fd4a15f550a70b3484e0a563130e
Derek Hong <derek.hong@paccar.com>
2024-04-16 15:30:15 -0700

    new updates

--- Lecture06/Lecture06.java
+++ Lecture06/Lecture06.java
@@ -9,47 +9,52 @@ class Lecture06 {
         // Note: you may need to put some of your answers inside comments
 
         // Put your answer for #1 here:
-            /*
-            Recursion is when a method calls on itself to solve smaller occurances of the same problem
-            It differs from a standard iterative method because it repeats upon itself until the case is solved directly
-            */
+        /*
+         * Recursion is when a method calls on itself to solve smaller occurances of the
+         * same problem
+         * It differs from a standard iterative method because it repeats upon itself
+         * until the case is solved directly
+         */
         // Put your answer for #2 here:
-            /*
-            Base case: The condition where recursion ends. The method needs to know when to end and the base case keeps it from running forever
-            Recursive case: This part calls on itself to bring itself closer to the base case
-            */
+        /*
+         * Base case: The condition where recursion ends. The method needs to know when
+         * to end and the base case keeps it from running forever
+         * Recursive case: This part calls on itself to bring itself closer to the base
+         * case
+         */
         // Put your answer for #3 here:
-        // Part A:
+        // Part A: 1
 
-        // Part B:
+        // Part B: 1, 2
 
-        // Part C:
+        // Part C: 1, 3
 
-        // Part D:
+        // Part D: 1, 2, 4
 
-        // Part E:
+        // Part E: 1, 2, 4, 8, 16
 
-        // Part F:
+        // Part F: 1, 3, 7, 15, 30
 
-        // Part G:
+        // Part G: 1, 3, 6, 12, 25, 50, 100
 
         // Put your answer for #5 here:
-        // Part A:
+        // Part A: *
 
-        // Part B:
+        // Part B: [*]
 
-        // Part C:
+        // Part C: ([*])
 
-        // Part D:
+        // Part D: ([([*])])
 
         // Put your answer for #6 here:
-        // Part A:
+        // Part A: 4
 
-        // Part B:
+        // Part B: 8, 4, 8
 
-        // Part C:
+        // Part C: 16, 8, 16
 
-        // Part D:
+        // Part D: 12, 8, 4, 8, 12
 
     }
+
 }
