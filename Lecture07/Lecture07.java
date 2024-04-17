c97f4fb815e60247c88eaba21a7dc64d18fb9048
Derek Hong <derek.hong@paccar.com>
2024-04-17 15:32:53 -0700

    new assignments added

---
+++ Lecture07/Lecture07.java
@@ -0,0 +1,31 @@
+class Lecture07 {
+    public static void main(String[] args) {
+        System.out.println("Hello from lecture 07");
+
+        // SELF-CHECK PROBLEMS:
+        // Note: you may need to put some of your answers inside comments
+
+        // Put your answer for #8 here:
+
+        // Put your answer for #9 here:
+
+        // Put your answer for #10 here:
+
+        // Put your answer for #11 here:
+
+        // Put your answer for #17 here:
+
+        // EXERCISES:
+
+        // Put your answer for #1 here:
+
+        // Put your answer for #2 here:
+
+        // Put your answer for #3 here:
+
+        // Put your answer for #10 here:
+
+        // Put your answer for #12 here:
+
+    }
+}
---
+++ Lecture08/Lecture08.java
@@ -0,0 +1,75 @@
+class Lecture08 {
+    public static void main(String[] args) {
+        System.out.println("Hello from lecture 08");
+
+        // SELF-CHECK PROBLEMS:
+        // Section 13.1: Searching and Sorting in the Java Class Libraries
+        // 3. Should you use a sequential or binary search on an array of Point objects,
+        // and why?
+
+        // 4. Under what circumstances can the Arrays.binarySearch and
+        // Collections.binarySearch methods be used successfully?
+
+        // 5. In what order does the Collections.sort method arrange a list of strings?
+        // How could you arrange them into a different order?
+
+        // Section 13.2: Program Complexity
+        // 9. Approximate the runtime of the following code fragment listed in Canvas
+
+        // 10. Approximate the runtime of the following code fragment listed in Canvas
+
+        // 11. Approximate the runtime of the following code fragment listed in Canvas
+
+        // Section 13.3: Implementing Searching and Sorting Algorithms
+        // 16. What is the runtime complexity class of a sequential search on an
+        // unsorted array?
+        // What is the runtime complexity class of the modified sequential search on a
+        // sorted array?
+
+        // 17. Why does the binary search algorithm require the input to be sorted?
+
+        // 18. How many elements (at most) does a binary search examine if the array
+        // contains 60 elements?
+
+        // 19. What indexes will be examined as the middle element by a binary search
+        // for the target value 8 when the search is run on the following input arrays?
+        // What value will the binary search algorithm return?
+
+        // A. int[] numbers = {1, 3, 6, 7, 8, 10, 15, 20, 30};
+        // B. int[] numbers = {1, 2, 3, 4, 5, 7, 8, 9, 10};
+        // C. int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // D. int[] numbers = {8, 9, 12, 14, 15, 17, 19, 25, 31};
+
+        // 20. What indexes will be examined as the middle element by a binary search
+        // for the target value 8 when the search is run on the following input array?
+        // Notice that the input array isn’t in sorted order. What can you say about the
+        // binary search algorithm’s result?
+
+        // int[] numbers = {6, 5, 8, 19, 7, 35, 22, 11, 9};
+
+        // 23. What modifications would you have to make to the selectionSort method to
+        // cause it to sort an array of double values rather than one of integer values?
+
+        // 24. Consider the following array:
+
+        // int[] numbers = {29, 17, 3, 94, 46, 8, –4, 12};
+        // After a single pass of the selection sort algorithm (a single swap), what
+        // would be the state of the array?
+
+        // A. {–4, 29, 17, 3, 94, 46, 8, 12}
+        // B. {29, 17, 3, 94, 46, 8, 12}
+        // C. {–4, 29, 17, 3, 94, 46, 8, –4, 12}
+        // D. {–4, 17, 3, 94, 46, 8, 29, 12}
+        // E. {3, 17, 29, 94, –4, 8, 46, 12}
+
+        // 25. Trace the execution of the selection sort algorithm as shown in this
+        // section when run on the following input arrays.
+        // Show each element that will be selected by the algorithm and where it will be
+        // moved, until the array is fully sorted.
+
+        // A. {29, 17, 3, 94, 46, 8, –4, 12}
+        // B. {33, 14, 3, 95, 47, 9, –42, 13}
+        // C. {7, 1, 6, 12, –3, 8, 4, 21, 2, 30, –1, 9}
+        // D. {6, 7, 4, 8, 11, 1, 10, 3, 5, 9}
+    }
+}
