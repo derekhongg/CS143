f1b55dd195b2eca4de5496ad4df139ce692c22c5
Derek Hong <derek.hong@paccar.com>
2024-04-22 15:03:27 -0700

    new updates

--- Lecture07/Lecture07.java
+++ Lecture07/Lecture07.java
@@ -6,15 +6,30 @@ class Lecture07 {
         // Note: you may need to put some of your answers inside comments
 
         // Put your answer for #8 here:
-
+            /*
+            - A call stack is a data structure used to keep track of functions, methods and procedures in a program.
+            - It organizes inforation about the active routines of a program and is needed to manage the execution of fuction calls
+            - It relates to recursion because recursion relies on the call stack to keep track of the different states of the recursive function calls
+            - Each recursive call adds a layer to the stack, and each return peels a layer off
+            */
         // Put your answer for #9 here:
-
+            /*
+            - The output would just return in the same order, no changes
+            */
         // Put your answer for #10 here:
-
+            /*
+            - Infinite loop because it calls itself before reading the next line
+            */
         // Put your answer for #11 here:
-
+            /*
+            - There is no base case
+            - Can be an issue if y is 0
+            */
         // Put your answer for #17 here:
-
+            /*
+            - Need to change it to < 10 in the base case
+            - This will keep running forever in the case that it is less than 10
+            */
         // EXERCISES:
 
         // Put your answer for #1 here:
