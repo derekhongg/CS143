5a4688268b9b1a39953d74de78965b8619e5b2b6
derekhongg <hongderek@gmail.com>
2024-05-27 09:57:09 -0700

    new work added

--- Lecture18/Lecture18.java
+++ Lecture18/Lecture18.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Lecture18 {
     public static void main(String[] args) {
         System.out.println("Hello from lecture 18");
@@ -12,6 +14,26 @@ class Lecture18 {
         // EXERCISES:
 
         // Put your answer for #21 here:
+        
+        /* 
+            See below
+        */
+    }
 
+    public int matches(IntTree other) {
+        return matches(overallRoot, other.overallRoot);
+    }
+
+    private int matches(IntTreeNode root1, IntTreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return 0;
+        } else {
+            int sum = matches(root1.left, root2.left) + matches(root1.right, root2.right);
+            if (root1.data == root2.data) {
+                return 1 + sum;
+            } else {
+                return sum;
+            }
+        }
     }
 }
